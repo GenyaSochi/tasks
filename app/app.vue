772b7e260505db2a17da553f3883a94489af66de
GenyaSochi <eunasedkina@gmail.com>
2025-11-23 21:54:56 +0300

    _

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Task1Component from './components/Task1Component.vue';
+
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <Task1Component />
+    <Task2Component />
+  </div>
+</template>
